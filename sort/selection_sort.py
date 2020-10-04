6df88a9ee760dc28e265538c1721a90a5b0de521
vimarsh2810 <31775752+vimarsh2810@users.noreply.github.com>
2020-10-04 23:31:58 +0530

    Create selection_sort.py

---
+++ sort/selection_sort.py
@@ -0,0 +1,11 @@
+arr = [3,5,1,2,4]
+print("Before sorting: ", arr)
+
+for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[min_idx] :
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print(arr)
